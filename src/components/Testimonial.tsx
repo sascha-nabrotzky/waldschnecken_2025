96c8826866ec682ad1a776ea10f10bc6c08801bf
Sascha Nabrotzky <sascha.nabrotzky@online.de>
2025-05-27 08:49:33 +0200

    fix: remove aria label, add svg title at testimonials

--- src/components/Testimonial.tsx
+++ src/components/Testimonial.tsx
@@ -18,10 +18,7 @@ const Testimonial: React.FC<Props> = ({ text, name, rating }) => {
             <p className="mx-auto -mt-6 max-w-xl text-xl">{text}</p>
             <p className="mx-auto mt-4 italic">{name}</p>
             {rating && (
-                <div
-                    className="mt-6 flex justify-center gap-1 text-whiskey-400"
-                    aria-label={`Google Bewertung ${rating} von 5 Sternen`}
-                >
+                <div className="mt-6 flex justify-center gap-1 text-whiskey-400">
                     {[...Array(rating)].map((_, index) => (
                         <svg
                             key={index}
@@ -31,6 +28,7 @@ const Testimonial: React.FC<Props> = ({ text, name, rating }) => {
                             viewBox="0 0 24 24"
                             fill="currentColor"
                         >
+                            <title>Filled star</title>
                             <path d="M12 .587l3.668 7.568 8.332 1.214-6.042 5.885 1.428 8.316L12 18.896l-7.386 3.87 1.428-8.316-6.042-5.885 8.332-1.214L12 .587z" />
                         </svg>
                     ))}
